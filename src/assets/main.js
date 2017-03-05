4dbda441343229efcb121ec0dc8dee72b3147e81
mariealexie <majs@hawaii.edu>
2017-03-05 09:15:40 -1000

    Changes to jQueryBadgesProject

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/mariealexie.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+
+    }
+  });
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(courses) {
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+
+    })
+  }
 
 });
